1bb1bffaa70a925e89a1d51cafd0dce7d9adee33
salihgerdan <8333520+salihgerdan@users.noreply.github.com>
2026-06-22 05:45:50 +0300

    Tune update period to 60 ms, seems smooth enough. Needs more testing on different devices

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,8 @@ use std::time::Duration;
 
 pub const APP_NAME: &str = "com.github.salihgerdan.spaceman";
 pub const APP_TITLE: &str = "SpaceMan";
-pub const UPDATE_PERIOD: Duration = Duration::from_millis(100); // used to batch add entries into the file tree
+// used to batch add entries into the file tree and update gui
+pub const UPDATE_PERIOD: Duration = Duration::from_millis(60);
 
 // only affects the treemap view
 pub const MAX_VISIBLE_FS_DEPTH: usize = 16;
